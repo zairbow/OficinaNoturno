89716e748c83cac2628bfda80ec9da2e880f9d7a
zairbow <vabeneti99@gmail.com>
2025-09-03 20:12:29 -0300

    Criação da main.py e da index.html

---
+++ main.py
@@ -0,0 +1,7 @@
+seuTime = (input("Qual é o maior time do mundo? "))
+
+if seuTime == "santos":
+    print("SANTOS É O TIME DA VIRADA, SANTOS É O TIME DO AMOR")
+else:
+    print("FALSO, SEU ANTI LIXO")
+
